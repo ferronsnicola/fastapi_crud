d5ce1812de0247f3bf860827bbd1d9c8b57055f2
ferronsnicola <ferronsnicola@gmail.com>
2023-07-13 16:46:14 +0200

    #dev app.api.routers.py implemented update_sensor as put method

--- app/api/routers.py
+++ app/api/routers.py
@@ -22,10 +22,18 @@ def get_sensor(sensor_id: int):
         raise HTTPException(status_code=404, detail="Sensor not found")
     return sensor
 
-@router.put("/sensors/{sensor_id}")
+@router.put("/sensors/{sensor_id}", description='Updates the specified sensor fields according to the specified sensor ID, raises HttpException if there is no sensor with specified ID, return the modified sensor as succesfull response')
 def update_sensor(sensor_id: int, sensor: ApiSensor):
-    # TODO
-    return None
+    db = SessionLocal()
+    existing_sensor = db.query(DbSensor).filter(DbSensor.id == sensor_id).first()
+    if not existing_sensor:
+        raise HTTPException(status_code=404, detail="Sensor not found")
+    existing_sensor.name = sensor.name
+    existing_sensor.ip_address = sensor.ip_address
+    existing_sensor.last_value = sensor.last_value
+    db.commit()
+    db.refresh(existing_sensor)
+    return existing_sensor
 
 @router.delete("/sensors/{sensor_id}")
 def delete_sensor(sensor_id: int):
