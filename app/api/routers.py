57682c4fa43f1dce87a7f4cc79eea32ee7dbdab0
ferronsnicola <ferronsnicola@gmail.com>
2023-07-13 16:41:39 +0200

    #fix app.api.routers.py removed response_model from annotations

--- app/api/routers.py
+++ app/api/routers.py
@@ -5,7 +5,7 @@ from app.db.models import Sensor as DbSensor
 router = APIRouter()
 SessionLocal = None
 
-@router.post("/sensors/", status_code=201, response_model=ApiSensor, description='Creates a new sensor data in the database, and returns it as a succesfull response')
+@router.post("/sensors/", status_code=201, description='Creates a new sensor data in the database, and returns it as a succesfull response')
 def create_sensor(sensor: ApiSensor):
     db = SessionLocal()
     new_sensor = DbSensor(name=sensor.name, ip_address=sensor.ip_address, last_value=sensor.last_value)
@@ -14,8 +14,8 @@ def create_sensor(sensor: ApiSensor):
     db.refresh(new_sensor)
     return new_sensor
 
-@router.get("/sensors/{sensor_id}")
-def get_sensor(sensor_id: int, response_model=ApiSensor, description='Retrieve sensor by ID, raises HttpException if there is no sensor with specified ID'):
+@router.get("/sensors/{sensor_id}", description='Retrieve sensor by ID, raises HttpException if there is no sensor with specified ID')
+def get_sensor(sensor_id: int):
     db = SessionLocal()
     sensor = db.query(DbSensor).filter(DbSensor.id == sensor_id).first()
     if not sensor:
