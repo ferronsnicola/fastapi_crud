18de69db51c73d0d49a266ad98778ebd68b81e23
ferronsnicola <ferronsnicola@gmail.com>
2023-07-13 15:53:59 +0200

    #dev app.api.routers.py skeleton of routers, missing implementation

--- app/api/routers.py
+++ app/api/routers.py
@@ -3,3 +3,24 @@ from app.api.models import SensorCreate, SensorUpdate
 from app.db.models import Sensor
 
 router = APIRouter()
+
+
+@router.post("/sensors/", status_code=201)
+def create_sensor(sensor: SensorCreate):
+    # TODO
+    return None
+
+@router.get("/sensors/{sensor_id}")
+def get_sensor(sensor_id: int):
+    # TODO
+    return None
+
+@router.put("/sensors/{sensor_id}")
+def update_sensor(sensor_id: int, sensor: SensorUpdate):
+    # TODO
+    return None
+
+@router.delete("/sensors/{sensor_id}")
+def delete_sensor(sensor_id: int):
+    # TODO
+    return None
